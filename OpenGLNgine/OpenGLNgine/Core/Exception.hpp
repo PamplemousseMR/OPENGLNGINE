5e6c43b41a6932c82709db569632bd030ad897b5
Romain MANCIAUX <romain.manciaux@gmail.com>
2021-10-23 20:32:17 +0200

    fix(Exception): compile on linux

--- OpenGLNgine/OpenGLNgine/Core/Exception.hpp
+++ OpenGLNgine/OpenGLNgine/Core/Exception.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <cassert>
-#include <exception>
+#include <stdexcept>
 #include <string>
 
 #define GLNGINE_ASSERT_IF(_cond, _message) assert(!(_cond) && (_message))
 
 #define GLNGINE_ASSERT(_message) GLNGINE_ASSERT_IF(false, _message)
 
-#define GLNGINE_EXCEPTION(_message) throw std::exception((std::string("[") + __FUNCTION__ + "] " + _message + ".").c_str())
+#define GLNGINE_EXCEPTION(_message) throw std::runtime_error ((std::string("[") + __FUNCTION__ + "] " + _message + ".").c_str())
 
 #ifdef NDEBUG
 #define GLNGINE_CHECK_GL
@@ -20,7 +20,7 @@
             std::string message = std::string((char*)gluErrorString(code)); \
             while((code = glGetError()) != GL_NO_ERROR) \
                 message += " " + std::string((char*)gluErrorString(code)); \
-            throw std::exception((std::string("[") + __FUNCTION__ + "] OpenGL error: " + message + ".").c_str()); \
+            throw std::runtime_error((std::string("[") + __FUNCTION__ + "] OpenGL error: " + message + ".").c_str()); \
         } \
     }
 #endif
